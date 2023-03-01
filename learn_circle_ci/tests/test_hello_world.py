e45f07600389c4a2cdfb22a181a8cc9a15469899
ppjad06 <114787520+ppjad06@users.noreply.github.com>
2023-03-01 12:41:20 -0500

    adding test for hello_world

--- learn_circle_ci/tests/test_hello_world.py
+++ learn_circle_ci/tests/test_hello_world.py
@@ -1,1 +1,6 @@
-
+import unittest
+class HelloWorldTest(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(print_hello_world("PJ"), "From PJ, Hello World!")
+if __name__ == "__main__":
+    unittest.main()
