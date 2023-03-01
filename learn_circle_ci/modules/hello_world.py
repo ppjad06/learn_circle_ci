5bf3613c8916a05feebb9bd6d018da0b2ce2cd52
ppjad06 <114787520+ppjad06@users.noreply.github.com>
2023-03-01 12:41:45 -0500

    Update hello_world.py

--- learn_circle_ci/modules/hello_world.py
+++ learn_circle_ci/modules/hello_world.py
@@ -1,5 +1,7 @@
 def print_hello_world(user:str)->None:
-    print(f"From {user}, Hello World!")
+    message = f"From {user}, Hello World!"
+    print(message)
+    return message
 
 if __name__ == "__main__":
     print_hello_world("priyanka")
