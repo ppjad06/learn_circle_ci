899572b77ffb8394a36ae7c1120cfc8cf207802e
ppjad06 <114787520+ppjad06@users.noreply.github.com>
2023-03-01 13:09:46 -0500

    Update test_hello_world.py

--- learn_circle_ci/tests/test_hello_world.py
+++ learn_circle_ci/tests/test_hello_world.py
@@ -1,4 +1,6 @@
 import unittest
+from learn_circle_ci.modules.hello_world import print_hello_world
+
 class HelloWorldTest(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(print_hello_world("PJ"), "From PJ, Hello World!")
